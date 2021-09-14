93f8174f1e2d7aa56344244d7323b354016b6e3c
techdecline <cornelius.schuchardt@gmail.com>
2021-09-14 18:49:49 +0000

    fix index in output

--- outputs.tf
+++ outputs.tf
@@ -9,8 +9,7 @@ output "gitlab_vm_ip_address" {
 
 output "secrets" {
   value = {
-    secret_name_public_key  = azurerm_key_vault_secret.secret-gitlab-pub.name
-    secret_name_private_key = azurerm_key_vault_secret.secret-gitlab-priv.name
+    secret_name_public_key  = azurerm_key_vault_secret.secret-gitlab-pub[0].name
+    secret_name_private_key = azurerm_key_vault_secret.secret-gitlab-priv[0].name
   }
-  
 }
